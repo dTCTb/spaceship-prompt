61f6392391a57ff547676b7948d9796dc3c38119
TCT <tctdonaldtang@gmail.com>
2019-06-09 04:29:38 +0800

    Ignore .DS_Store

---
+++ custom/my_spaceship.zsh
@@ -0,0 +1,62 @@
+SPACESHIP_PROMPT_ORDER=(
+    venv
+    dir    
+    git
+    user
+    host
+    char
+)
+
+# Prompt
+SPACESHIP_PROMPT_ADD_NEWLINE=false
+SPACESHIP_PROMPT_SEPARATE_LINE=false
+
+# Char
+# SPACESHIP_CHAR_SYMBOL="π ❯ "
+SPACESHIP_CHAR_SYMBOL="π "
+SPACESHIP_CHAR_SYMBOL_ROOT="!❯ "
+SPACESHIP_CHAR_COLOR_SUCCESS="magenta"
+SPACESHIP_CHAR_COLOR_FAILURE="red"
+SPACESHIP_CHAR_COLOR_SECONDARY="yellow"
+
+# Username (user)
+SPACESHIP_USER_SHOW=true
+SPACESHIP_USER_PREFIX=""
+SPACESHIP_USER_SUFFIX=""
+
+# Hostname (host)
+SPACESHIP_HOST_PREFIX="@"
+SPACESHIP_HOST_COLOR_SSH="yellow"
+
+# Directory (dir)
+SPACESHIP_DIR_PREFIX=" "
+SPACESHIP_DIR_TRUNC=0
+# SPACESHIP_DIR_TRUNC_PREFIX="…/"
+SPACESHIP_DIR_COLOR="blue"
+
+# Git (git)
+SPACESHIP_GIT_PREFIX="@ "
+
+    # Git Branch (git_branch)
+    SPACESHIP_GIT_BRANCH_COLOR="green"
+
+    # Git Status (git_status)
+    SPACESHIP_GIT_STATUS_PREFIX=" "
+    SPACESHIP_GIT_STATUS_SUFFIX=""
+    # ? Untracked changes
+    # + Added changes
+    # ! Unstaged files
+    # » Renamed files
+    # ✘ Deleted files
+    # $ Stashed changes
+    # = Unmerged changes
+    # ⇡ Unpushed changes (ahead of remote branch)
+    # ⇣ Unpulled changes (behind of remote branch)
+    # ⇕ Diverged changes (diverged with remote branch)
+
+# Virtualenv (venv)
+SPACESHIP_VENV_SHOW=true
+SPACESHIP_VENV_PREFIX=""
+SPACESHIP_VENV_SUFFIX=")"
+SPACESHIP_VENV_SYMBOL=" ("
+SPACESHIP_VENV_COLOR="white"
